1d333a6ad4ec2718931480ce908806c9f6335f77
Aditya Agarwal <adityaa803@gmail.com>
2020-12-25 23:38:16 +0530

    added a11y addon

--- packages/docs/src/components/FilmCard.js
+++ packages/docs/src/components/FilmCard.js
@@ -3,7 +3,7 @@ import { tw } from 'twind';
 export function FilmCard({ film }) {
   return (
     <article className={tw`w-72 bg-indigo-100 rounded-md border-2 border-indigo-200 px-4 py-5`}>
-      <h4 className={tw`font-bold text-blue-500 text-2xl mb-3`}>{film.title}</h4>
+      <h4 className={tw`font-bold text-blue-800 text-2xl mb-3`}>{film.title}</h4>
       <p>{film.opening_crawl}</p>
     </article>
   );
