273f59c7277dd20e1e924412b652bb3fcde06db5
Aditya Agarwal <adityaa803@gmail.com>
2021-01-09 14:52:40 +0530

    non-working: added responsive addon

--- packages/docs/.storybook/preview.js
+++ packages/docs/.storybook/preview.js
@@ -1,10 +1,18 @@
 import { addDecorator } from '@storybook/react';
 import { initializeWorker, mswDecorator } from 'msw-storybook-addon';
+import { withResponsiveViews } from 'storybook-addon-responsive-views';
 
 import '../src/styles.css';
 
 initializeWorker();
 addDecorator(mswDecorator);
+addDecorator(
+  withResponsiveViews({
+    mobile: 425,
+    tablet: 750,
+    desktop: 1000,
+  }),
+);
 
 export const parameters = {
   options: {
