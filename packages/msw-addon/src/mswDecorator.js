661d213c924625da84a21c238fa5aea1ea7899a0
Yann Braga <yannbf@gmail.com>
2021-05-07 17:27:15 +0200

    feat: add node support
    - When not in the browser, the addon will fall back to setting up the server

--- packages/msw-addon/src/mswDecorator.js
+++ packages/msw-addon/src/mswDecorator.js
@@ -1,5 +1,5 @@
-import { makeDecorator } from '@storybook/addons';
 import { setupWorker } from 'msw';
+import { setupServer } from 'msw/node';
 
 let worker;
 
@@ -7,17 +7,25 @@ export function initializeWorker(options) {
   if (typeof global.process === 'undefined') {
     worker = setupWorker();
     worker.start(options);
+  } else {
+    worker = setupServer();
+    worker.listen(options);
   }
 }
 
-export const mswDecorator = makeDecorator({
-  name: 'withMsw',
-  parameterName: 'msw',
-  wrapper: (storyFn, context, { parameters = [] }) => {
-    if (worker) {
-      worker.resetHandlers();
-      worker.use(...parameters);
+export const mswDecorator = (storyFn, { parameters: { msw = [] } }) => {
+  if (worker) {
+    worker.resetHandlers();
+
+    if (Array.isArray(msw)) {
+      if (msw.length > 0) {
+        worker.use(...msw);
+      }
+    } else {
+      throw new Error(`[MSW] expected to receive an array of handlers but received "${typeof msw}" instead.
+        Please refer to the documentation: https://mswjs.io/docs/getting-started/mocks/rest-api`)
     }
-    return storyFn(context);
-  },
-});
+  }
+
+  return storyFn();
+};
