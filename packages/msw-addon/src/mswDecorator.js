5660c74b0f1f49421408fef9d65108ac74de7197
Yann Braga <yannbf@gmail.com>
2021-05-08 14:45:57 +0200

    feat: expose worker
    - for scenarios were people want to tinker with the worker, such as in tests

--- packages/msw-addon/src/mswDecorator.js
+++ packages/msw-addon/src/mswDecorator.js
@@ -3,14 +3,24 @@ import { setupServer } from 'msw/node';
 
 let worker;
 
+const IS_BROWSER = typeof global.process === 'undefined'
+
 export function initializeWorker(options) {
-  if (typeof global.process === 'undefined') {
+  if (IS_BROWSER) {
     worker = setupWorker();
     worker.start(options);
   } else {
     worker = setupServer();
     worker.listen(options);
   }
+}
+
+export function getWorker() {
+  if(worker === undefined) {
+    throw new Error(`[MSW] Tried to get worker but it was not defined yet. Did you forget to initialize it?`)
+  }
+
+  return worker;
 }
 
 export const mswDecorator = (storyFn, { parameters: { msw = [] } }) => {
