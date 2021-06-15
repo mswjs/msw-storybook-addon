4fd20a417c2b964eb390adf67ac61a85a7badf03
Yann Braga <yannbf@gmail.com>
2021-06-15 17:05:43 +0200

    Supports running add-on in Node.js (#27)
    * chore: add development script for msw and sb
    
    * chore(docs): use CSF syntax in preview file
    
    * chore(docs): link dev dependency for better dx
    
    - this allows building the msw addon package and having direct impact
    in the docs, with HMR
    
    * feat: add node support
    
    - When not in the browser, the addon will fall back to setting up the server
    
    * feat: expose worker
    
    - for scenarios were people want to tinker with the worker, such as in tests
    
    * test: add example reusing the addon in stories
    
    - with @storybook/testing-react
    
    * test(docs): run addon in node without @storybook/testing-react
    
    * refactor: check for error scenario first in decorator
    
    * refactor: create class to proxy msw implementation
    
    * fix: update error message link
    
    - point to /mock instead, we don't know if the user uses graphql or rest
    
    * feat: turn initializeWorker to initialize
    
    * docs: add contributing guidelines
    
    * Rephrases the "no worker found" error message
    
    * fix: remove duplicate dependency
    
    Co-authored-by: Artem Zakharchenko <kettanaito@gmail.com>

--- packages/msw-addon/src/mswDecorator.js
+++ packages/msw-addon/src/mswDecorator.js
@@ -1,23 +1,53 @@
-import { makeDecorator } from '@storybook/addons';
-import { setupWorker } from 'msw';
+let api
 
-let worker;
+const IS_BROWSER = typeof global.process === 'undefined'
 
 export function initializeWorker(options) {
-  if (typeof global.process === 'undefined') {
-    worker = setupWorker();
-    worker.start(options);
-  }
+  console.warn(
+    `[MSW] "initializeWorker" is now deprecated, please use "initialize" instead. This method will be removed in future releases.`
+  )
+  return initialize(options)
 }
 
-export const mswDecorator = makeDecorator({
-  name: 'withMsw',
-  parameterName: 'msw',
-  wrapper: (storyFn, context, { parameters = [] }) => {
-    if (worker) {
-      worker.resetHandlers();
-      worker.use(...parameters);
+export function initialize(options) {
+  if (IS_BROWSER) {
+    const { setupWorker } = require('msw')
+    const worker = setupWorker()
+    worker.start(options)
+    api = worker
+  } else {
+    const { setupServer } = require('msw/node')
+    const server = setupServer()
+    server.listen(options)
+    api = server
+  }
+
+  return api
+}
+
+export function getWorker() {
+  if (api === undefined) {
+    throw new Error(
+      `[MSW] Failed to retrieve the worker: no active worker found. Did you forget to call "initialize"?`
+    )
+  }
+
+  return api
+}
+
+export const mswDecorator = (storyFn, { parameters: { msw = [] } }) => {
+  if (api) {
+    api.resetHandlers()
+
+    if (!Array.isArray(msw)) {
+      throw new Error(`[MSW] expected to receive an array of handlers but received "${typeof msw}" instead.
+        Please refer to the documentation: https://mswjs.io/docs/getting-started/mocks/`)
     }
-    return storyFn(context);
-  },
-});
+
+    if (msw.length > 0) {
+      api.use(...msw)
+    }
+  }
+
+  return storyFn()
+}
