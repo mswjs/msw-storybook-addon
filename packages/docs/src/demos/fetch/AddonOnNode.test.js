1bd0c2b4dd05f7a69cf0e6f2dcdac97d75654d9c
Artem Zakharchenko <kettanaito@gmail.com>
2021-10-07 13:26:19 +0200

    Updates "msw" to 0.35.0 (#32)
    * Updates "msw" to 0.30.0
    
    * Updates "msw" to 0.35.0
    
    * test: cleanup test logs
    
    * chore: add build step to CI
    
    * test: restore console mocks, use ".toHaveLength" assertions
    
    Co-authored-by: Yann Braga <yannbf@gmail.com>

--- packages/docs/src/demos/fetch/AddonOnNode.test.js
+++ packages/docs/src/demos/fetch/AddonOnNode.test.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import { render, waitFor, screen } from '@testing-library/react';
+import React from 'react'
+import { render, waitFor, screen } from '@testing-library/react'
 
-import { initialize, mswDecorator } from 'msw-storybook-addon';
-import { MockedSuccess, MockedError } from './App.stories';
+import { initialize, mswDecorator } from 'msw-storybook-addon'
+import { MockedSuccess, MockedError } from './App.stories'
 
 // Useful in scenarios where the addon runs on node, such as with @storybook/testing-react
 describe('Running msw-addon on node', () => {
@@ -18,27 +18,30 @@ describe('Running msw-addon on node', () => {
 
   it('renders film cards for each film', async () => {
     // Story + msw addon decorator, which resets and applies the server handlers based on story parameters
-    render(mswDecorator(MockedSuccess, { parameters: MockedSuccess.parameters }));
+    render(
+      mswDecorator(MockedSuccess, { parameters: MockedSuccess.parameters })
+    )
 
-    expect(screen.getByText(/fetching star wars data/i)).toBeInTheDocument();
+    expect(screen.getByText(/fetching star wars data/i)).toBeInTheDocument()
 
-    await waitFor(() => screen.getAllByRole('article'));
+    await waitFor(() => screen.getAllByRole('article'))
 
-    const articleNodes = screen.getAllByRole('article');
-    expect(articleNodes.length).toEqual(3);
+    const articleNodes = screen.getAllByRole('article')
+    expect(articleNodes).toHaveLength(3)
 
-    const headingNodes = screen.getAllByRole('heading');
-    expect(headingNodes[0]).toHaveTextContent('A New Hope');
-    expect(headingNodes[1]).toHaveTextContent('Empire Strikes Back');
-    expect(headingNodes[2]).toHaveTextContent('Return of the Jedi');
-  });
+    const headingNodes = screen.getAllByRole('heading')
+    expect(headingNodes[0]).toHaveTextContent('A New Hope')
+    expect(headingNodes[1]).toHaveTextContent('Empire Strikes Back')
+    expect(headingNodes[2]).toHaveTextContent('Return of the Jedi')
+  })
 
   it('renders error message if it cannot load the films', async () => {
     // Story + msw addon decorator, which resets and applies the server handlers based on story parameters
-    render(mswDecorator(MockedError, { parameters: MockedError.parameters }));
+    render(mswDecorator(MockedError, { parameters: MockedError.parameters }))
 
-    const errorMsgNode = await screen.findByText(/could not fetch star wars data/i);
-    expect(errorMsgNode).toBeInTheDocument();
-  });
-
+    const errorMsgNode = await screen.findByText(
+      /could not fetch star wars data/i
+    )
+    expect(errorMsgNode).toBeInTheDocument()
+  })
 })
--- packages/docs/src/demos/react-query/App.test.js
+++ packages/docs/src/demos/react-query/App.test.js
@@ -1,32 +1,41 @@
-import React from 'react';
-import { render, waitFor, screen } from '@testing-library/react';
+import React from 'react'
+import { render, waitFor, screen } from '@testing-library/react'
 
-import { getServer } from '../../test-utils';
-import { MockedSuccess, MockedError } from './App.stories';
+import { getServer } from '../../test-utils'
+import { MockedSuccess, MockedError } from './App.stories'
 
-const server = getServer();
+const server = getServer()
+
+afterAll(() => {
+  jest.restoreAllMocks()
+})
 
 it('renders film cards for each film', async () => {
-  server.use(...MockedSuccess.parameters.msw);
-  render(<MockedSuccess />);
+  server.use(...MockedSuccess.parameters.msw)
+  render(<MockedSuccess />)
 
-  expect(screen.getByText(/fetching star wars data/i)).toBeInTheDocument();
+  expect(screen.getByText(/fetching star wars data/i)).toBeInTheDocument()
 
-  await waitFor(() => screen.getAllByRole('article'));
+  await waitFor(() => screen.getAllByRole('article'))
 
-  const articleNodes = screen.getAllByRole('article');
-  expect(articleNodes.length).toEqual(3);
+  const articleNodes = screen.getAllByRole('article')
+  expect(articleNodes).toHaveLength(3)
 
-  const headingNodes = screen.getAllByRole('heading');
-  expect(headingNodes[0]).toHaveTextContent('A New Hope');
-  expect(headingNodes[1]).toHaveTextContent('Empire Strikes Back');
-  expect(headingNodes[2]).toHaveTextContent('Return of the Jedi');
-});
+  const headingNodes = screen.getAllByRole('heading')
+  expect(headingNodes[0]).toHaveTextContent('A New Hope')
+  expect(headingNodes[1]).toHaveTextContent('Empire Strikes Back')
+  expect(headingNodes[2]).toHaveTextContent('Return of the Jedi')
+})
 
 it('renders error message if it cannot load the films', async () => {
-  server.use(...MockedError.parameters.msw);
-  render(<MockedError />);
+  // get rid of the console.error for this test which adds clutter to the logs
+  jest.spyOn(console, 'error').mockImplementationOnce(() => {})
 
-  const errorMsgNode = await screen.findByText(/could not fetch star wars data/i);
-  expect(errorMsgNode).toBeInTheDocument();
-});
+  server.use(...MockedError.parameters.msw)
+  render(<MockedError />)
+
+  const errorMsgNode = await screen.findByText(
+    /could not fetch star wars data/i
+  )
+  expect(errorMsgNode).toBeInTheDocument()
+})
