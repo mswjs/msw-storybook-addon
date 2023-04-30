a332ff718e53150bbe5ea0eb767869e6b9fa9c40
Yann Braga <yannbf@gmail.com>
2023-04-30 17:40:31 +0200

    remove unnecessary options [skip ci]

--- packages/docs/.storybook/preview.js
+++ packages/docs/.storybook/preview.js
@@ -9,16 +9,6 @@ initialize();
  */
 const preview = {
   parameters: {
-    options: {
-      storySort: {
-        order: ['Guides', ['Introduction', 'Installation'], 'Demos', ['Urql']],
-      },
-    },
-    previewTabs: {
-      'storybook/docs/panel': {
-        hidden: true,
-      },
-    },
     actions: { argTypesRegex: '^on[A-Z].*' },
   },
   loaders: [mswLoader]
