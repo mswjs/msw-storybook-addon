12a47e0b3cff974f6f6904cf905684e8b9b3f395
Yann Braga <yannbf@gmail.com>
2023-10-24 09:31:44 +0200

    small update

--- packages/docs/.storybook/main.ts
+++ packages/docs/.storybook/main.ts
@@ -4,24 +4,19 @@ import { StorybookConfig } from "@storybook/react-vite";
 import { mergeConfig } from "vite";
 
 const config: StorybookConfig = {
-  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
 
   addons: [
     getAbsolutePath("@storybook/addon-links"),
     getAbsolutePath("@storybook/addon-essentials"),
     getAbsolutePath("@storybook/addon-interactions"),
     getAbsolutePath("@storybook/addon-a11y"),
-    getAbsolutePath("@storybook/addon-storysource"),
     getAbsolutePath("@storybook/addon-mdx-gfm"),
   ],
 
   framework: {
     name: getAbsolutePath("@storybook/react-vite") as '@storybook/react-vite',
     options: {},
-  },
-
-  features: {
-    storyStoreV7: true,
   },
 
   staticDirs: ["../public"],
