dcca74c9b7b185bd33e52bdf30ecca3e527f3bc6
Yann Braga <yannbf@gmail.com>
2024-03-27 11:10:58 +0100

    export applyRequestHandlers

--- packages/msw-addon/src/index.ts
+++ packages/msw-addon/src/index.ts
@@ -1,3 +1,8 @@
-export { initialize, InitializeOptions, getWorker } from '@build-time/initialize'
+export {
+  initialize,
+  InitializeOptions,
+  getWorker,
+} from '@build-time/initialize'
 export * from './decorator.js'
 export * from './loader.js'
+export { applyRequestHandlers } from './applyRequestHandlers.js'
