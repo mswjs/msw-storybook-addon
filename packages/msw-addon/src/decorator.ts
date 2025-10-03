0e1d6c499752f90f0591483684767434ccab1251
Connor Shea <connor.james.shea@gmail.com>
2025-10-02 19:19:18 -0600

    Add an `@deprecated` JSDoc tag to the mswDecorator.
    Previously, this was only communicated to the user via the browser console, and so was easy to miss. I only noticed it because of reading the changelog.
    
    It'll be much more likely for users to notice this deprecation if their editor tells them that it's deprecated, so this will hopefully help ease migration.

--- packages/msw-addon/src/decorator.ts
+++ packages/msw-addon/src/decorator.ts
@@ -21,6 +21,10 @@ const deprecateMessage = deprecate(`
 More info: https://github.com/mswjs/msw-storybook-addon/blob/main/MIGRATION.md#mswdecorator-is-deprecated-in-favor-of-mswloader
 `)
 
+/**
+ * @deprecated The `mswDecorator` is deprecated and will be removed in the next release. Please use the `mswLoader` instead.
+ * More info: https://github.com/mswjs/msw-storybook-addon/blob/main/MIGRATION.md#mswdecorator-is-deprecated-in-favor-of-mswloader
+ */
 export const mswDecorator = <Story extends (...args: any[]) => any>(
   storyFn: Story,
   context: Context
