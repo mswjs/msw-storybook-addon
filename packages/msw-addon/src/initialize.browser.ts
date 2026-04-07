c1bb0a71ef5864d19a90d9a98d181bf9ec3a9da5
Artem Zakharchenko <kettanaito@gmail.com>
2026-04-07 20:23:56 +0200

    chore: use explicit prettier config

--- packages/msw-addon/src/initialize.browser.ts
+++ packages/msw-addon/src/initialize.browser.ts
@@ -1,38 +1,38 @@
-import type { RequestHandler } from "msw";
-import { setupWorker } from "msw/browser";
-import { augmentInitializeOptions } from "./augmentInitializeOptions.js";
+import type { RequestHandler } from 'msw'
+import { setupWorker } from 'msw/browser'
+import { augmentInitializeOptions } from './augmentInitializeOptions.js'
 
-type SetupWorker = ReturnType<typeof setupWorker>;
+type SetupWorker = ReturnType<typeof setupWorker>
 
-export let api: SetupWorker;
+export let api: SetupWorker
 
-export type InitializeOptions = Parameters<SetupWorker["start"]>[0];
+export type InitializeOptions = Parameters<SetupWorker['start']>[0]
 
-let activationPromise: Promise<void>;
+let activationPromise: Promise<void>
 
 export function initialize(
   options?: InitializeOptions,
   initialHandlers: RequestHandler[] = []
 ): SetupWorker {
-  const worker = setupWorker(...initialHandlers);
+  const worker = setupWorker(...initialHandlers)
   activationPromise = worker
     .start(augmentInitializeOptions(options))
-    .then(() => {});
-  api = worker;
-  return worker;
+    .then(() => {})
+  api = worker
+  return worker
 }
 
 export async function waitForMswReady() {
-  getWorker();
-  await activationPromise;
+  getWorker()
+  await activationPromise
 }
 
 export function getWorker(): SetupWorker {
   if (api === undefined) {
     throw new Error(
       `[MSW] Failed to retrieve the worker: no active worker found. Did you forget to call "initialize"?`
-    );
+    )
   }
 
-  return api;
+  return api
 }
