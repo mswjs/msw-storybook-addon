b4c7a12745590a444556ccdc306b465d7fc4912b
Artem Zakharchenko <kettanaito@gmail.com>
2026-07-03 13:07:45 +0200

    chore: increase playwright test timeout

--- playwright.config.ts
+++ playwright.config.ts
@@ -6,51 +6,51 @@ const CUSTOM_SETUP_PORT = 56791
 
 export default defineConfig({
   testMatch: '*.test.ts',
-  timeout: 5000,
+  timeout: 10_000,
   forbidOnly: !!process.env.CI,
   reporter: 'list',
   use: {
     trace: 'on-first-retry',
-    serviceWorkers: 'allow',
+    serviceWorkers: 'allow'
   },
   projects: [
     {
       name: 'factory',
       testDir: './tests/factory',
       use: {
-        baseURL: `http://localhost:${DEFAULT_PORT}/`,
-      },
+        baseURL: `http://localhost:${DEFAULT_PORT}/`
+      }
     },
     {
       name: 'csf3',
       testDir: './tests/csf3',
       use: {
-        baseURL: `http://localhost:${CSF3_PORT}/`,
-      },
+        baseURL: `http://localhost:${CSF3_PORT}/`
+      }
     },
     {
       name: 'factory-custom-setup',
       testDir: './tests/factory-custom-setup',
       use: {
-        baseURL: `http://localhost:${CUSTOM_SETUP_PORT}/`,
-      },
-    },
+        baseURL: `http://localhost:${CUSTOM_SETUP_PORT}/`
+      }
+    }
   ],
   webServer: [
     {
       command: `pnpm storybook -p ${DEFAULT_PORT}`,
       port: DEFAULT_PORT,
-      reuseExistingServer: true,
+      reuseExistingServer: true
     },
     {
       command: `pnpm storybook:csf3 -p ${CSF3_PORT}`,
       port: CSF3_PORT,
-      reuseExistingServer: true,
+      reuseExistingServer: true
     },
     {
       command: `pnpm storybook:factory-custom-setup -p ${CUSTOM_SETUP_PORT}`,
       port: CUSTOM_SETUP_PORT,
-      reuseExistingServer: true,
-    },
-  ],
+      reuseExistingServer: true
+    }
+  ]
 })
